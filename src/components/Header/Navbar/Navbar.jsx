5ff3f3ef97c7c7438cf280a8272c4109c561a67e
ilya <ilya_ostapenko-2222@mail.ru>
2025-04-29 10:38:32 +0300

    header is aligned

--- src/components/Header/Navbar/Navbar.jsx
+++ src/components/Header/Navbar/Navbar.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import s from './Navbar.module.css';
+import settings from '../../images/settings_white.png'
+import support from '../../images/support_white.png'
+import userProfile from '../../images/user-profile_white.png'
 
 const Navbar = () => {
     return (
         <nav className={s.navbar}>
-            <span>Help</span>
-            <span>Profile</span>
-            <span>Settings</span>
+            <ul>
+                <li><img src={settings} alt="settings"/></li>
+                <li><img src={support} alt="support"/></li>
+                <li><img src={userProfile} alt="userProfile"/></li>
+            </ul>
         </nav>
     )
 }
