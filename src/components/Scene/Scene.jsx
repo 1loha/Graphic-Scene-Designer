2d9a482caaf73ef4c0f97cc1cce1891c70b2625b
ilya <ilya_ostapenko-2222@mail.ru>
2025-04-30 13:12:21 +0300

    added light, floor and scary sphere

--- src/components/Scene/Scene.jsx
+++ src/components/Scene/Scene.jsx
@@ -1,24 +1,41 @@
 // import React from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
+import { useRef } from "react";
+import { useLoader } from "@react-three/fiber";
+import { TextureLoader } from "three/src/loaders/TextureLoader";
+import texture from "./../images/grass.jpg";
 import s from './Scene.module.css';
 
 function Box() {
     return (
-        <mesh>
-            <boxGeometry args={[1, 1, 1]} />
+        <mesh position={[0, 0, 0]}>
+            <boxGeometry args={[10, .01, 10]} />
             <meshStandardMaterial color="orange" />
+        </mesh>
+    );
+}
+
+function Sphere() {
+    const textureMap = useLoader(TextureLoader, texture);
+    return (
+        <mesh position={[0, 2, 0]}>
+            <sphereGeometry args={[2, 32]} />
+            {/*<meshStandardMaterial color={0x00ff00} />*/}
+            <meshStandardMaterial map={textureMap} />
         </mesh>
     );
 }
 
 const Scene = () => {
     return (
-        <Canvas className={s.scene}>
-            <ambientLight intensity={1} />
-            <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-            <pointLight position={[-10, -10, -10]} />
+        <Canvas
+            className={s.scene}
+            camera={{fov: 80, position: [3, 5, 0]}}>
+            <ambientLight intensity={.1} />
+            <directionalLight position={[-2, 2, -2]} intensity={0.8} />
             <Box />
+            <Sphere />
             <OrbitControls /> {/* Позволяет вращать сцену мышкой */}
         </Canvas>
     );
