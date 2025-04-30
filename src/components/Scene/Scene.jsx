1de67c59287edef3d3ea97e52eb30c09a5231622
ilya <ilya_ostapenko-2222@mail.ru>
2025-04-30 13:41:16 +0300

    err load earth

--- src/components/Scene/Scene.jsx
+++ src/components/Scene/Scene.jsx
@@ -1,10 +1,12 @@
-// import React from 'react';
+import React from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import { useRef } from "react";
-import { useLoader } from "@react-three/fiber";
-import { TextureLoader } from "three/src/loaders/TextureLoader";
-import texture from "./../images/grass.jpg";
+// import { useLoader } from "@react-three/fiber";
+// import { TextureLoader } from "three/src/loaders/TextureLoader";
+// import texture from "./../images/grass.jpg";
+import { useGLTF } from "@react-three/drei";
+import Earth from "./Earth";
 import s from './Scene.module.css';
 
 function Box() {
@@ -16,16 +18,18 @@ function Box() {
     );
 }
 
-function Sphere() {
-    const textureMap = useLoader(TextureLoader, texture);
-    return (
-        <mesh position={[0, 2, 0]}>
-            <sphereGeometry args={[2, 32]} />
-            {/*<meshStandardMaterial color={0x00ff00} />*/}
-            <meshStandardMaterial map={textureMap} />
-        </mesh>
-    );
-}
+// function Sphere() {
+//     const textureMap = useLoader(TextureLoader, texture);
+//     return (
+//         <mesh position={[0, 2, 0]}>
+//             <sphereGeometry args={[2, 32]} />
+//             {/*<meshStandardMaterial color={0x00ff00} />*/}
+//             <meshStandardMaterial map={textureMap} />
+//         </mesh>
+//     );
+// }
+
+
 
 const Scene = () => {
     return (
@@ -35,7 +39,8 @@ const Scene = () => {
             <ambientLight intensity={.1} />
             <directionalLight position={[-2, 2, -2]} intensity={0.8} />
             <Box />
-            <Sphere />
+            <Earth />
+            {/*<Sphere />*/}
             <OrbitControls /> {/* Позволяет вращать сцену мышкой */}
         </Canvas>
     );
