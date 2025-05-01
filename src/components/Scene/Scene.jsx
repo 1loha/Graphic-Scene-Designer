a874c0a27fc2b8e869d6b062fc4b0288dab1d0ed
ilya <ilya_ostapenko-2222@mail.ru>
2025-05-01 15:47:29 +0300

    added grid and gltf models

--- src/components/Scene/Scene.jsx
+++ src/components/Scene/Scene.jsx
@@ -1,47 +1,56 @@
 import React from 'react';
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
-import { useRef } from "react";
-// import { useLoader } from "@react-three/fiber";
-// import { TextureLoader } from "three/src/loaders/TextureLoader";
-// import texture from "./../images/grass.jpg";
-import { useGLTF } from "@react-three/drei";
-import Earth from "./Earth";
+import { Canvas, useLoader } from '@react-three/fiber';
+import { OrbitControls, Grid, Plane, useOBJ, useGLTF } from '@react-three/drei';
 import s from './Scene.module.css';
 
-function Box() {
-    return (
-        <mesh position={[0, 0, 0]}>
-            <boxGeometry args={[10, .01, 10]} />
-            <meshStandardMaterial color="orange" />
-        </mesh>
-    );
+function Model(props) {
+    // относительный путь public
+    const { scene } = useGLTF(props.path);
+    return <primitive object={scene} scale={props.scale}
+                      position={props.position}
+                      rotation={props.rotation}
+    />;
 }
 
-// function Sphere() {
-//     const textureMap = useLoader(TextureLoader, texture);
-//     return (
-//         <mesh position={[0, 2, 0]}>
-//             <sphereGeometry args={[2, 32]} />
-//             {/*<meshStandardMaterial color={0x00ff00} />*/}
-//             <meshStandardMaterial map={textureMap} />
-//         </mesh>
-//     );
-// }
-
-
-
 const Scene = () => {
+
     return (
-        <Canvas
-            className={s.scene}
-            camera={{fov: 80, position: [3, 5, 0]}}>
-            <ambientLight intensity={.1} />
+        <Canvas className={s.scene}
+            camera={{fov: 80, position: [3, 5, 1]}}>
+            <ambientLight intensity={.5} />
             <directionalLight position={[-2, 2, -2]} intensity={0.8} />
-            <Box />
-            <Earth />
-            {/*<Sphere />*/}
-            <OrbitControls /> {/* Позволяет вращать сцену мышкой */}
+            <OrbitControls/>
+            <Grid
+                infiniteGrid // Бесконечная сетка
+                cellSize={0.2} // Размер ячейки
+                cellThickness={.6} // Толщина линий ячеек
+                cellColor="#bababa" // Цвет линий ячеек
+                sectionSize={5} // Размер секции
+                sectionThickness={.5} // Толщина секций
+                sectionColor="#777777" // Цвет секций
+                fadeDistance={30} // Расстояние исчезновения
+                fadeStrength={0} // Интенсивность исчезновения
+            />
+            <Model path='/steve/source/model.gltf'
+                   scale={0.5}
+                   position={[-1, 0, -2]}
+            />
+            <Model path='/shameless_loona_pose/scene.gltf'
+                scale={0.5}
+                position={[2, 0, 2]}
+            />
+            {/*<axesHelper args={[5]} />*/}
+
+            {/*<Plane*/}
+            {/*    // args={[30, 30]} // Размер*/}
+            {/*    // rotation={[-Math.PI / 2, 0, 0]}*/}
+            {/*    >*/}
+            {/*    <meshStandardMaterial*/}
+            {/*        color="#ffffff"*/}
+            {/*        roughness={0.8}*/}
+            {/*        metalness={0.2}*/}
+            {/*    />*/}
+            {/*</Plane>*/}
         </Canvas>
     );
 };
